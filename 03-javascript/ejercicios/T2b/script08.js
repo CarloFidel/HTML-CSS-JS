51668c8b29f72e42ffa01226a62d5183c2617b1d
Carlo Fidel Taboada Petersson <carloftaboada@gmail.com>
2025-01-29 01:25:57 +0100

    Cambios en Tb

--- 03-javascript/ejercicios/T2b/script08.js
+++ 03-javascript/ejercicios/T2b/script08.js
@@ -5,17 +5,18 @@ function retit() {
 setTimeout(retit, 3000);
 
 function gris() {
-  document.body.style.backgroundColor = "gray";
+  document.body.style.backgroundColor = "rgb(86, 86, 100)";
+  document.body.style.transition = "background-color 1s ease";
 }
 function blanco() {
-  document.body.style.backgroundColor = "white";
+  document.body.style.backgroundColor = "rgb(128, 128, 155)";
 }
 function textcolor() {
   document.getElementById("textblue").style.color = "blue";
   document.getElementById("textblue2").style.color = "blue";
 }
 
-///////////////////////////////////////////////////////////////
+//////////////////////////ejercicio09/////////////////////////////////////
 
 let amp = window.outerWidth;
 console.log(`amplada de pantalla es ${amp}`);
@@ -28,13 +29,11 @@ console.log(`alt util de pantalla es ${altuti}`);
 let dia = Math.sqrt(amputi * amputi + altuti * altuti);
 console.log(`diagonal de pantalla es ${dia}`);
 
-/////////////////////////////////////////////////////////////
-
 let win;
 function finestra() {
   win = window.open("novafinestra.html", "hey", "height=300, width=700");
 }
-/////////////////////////////////////////////////////////////
+//////////////////////////////ejercicio12///////////////////////////////
 function click() {
   let inp = document.getElementById("input").value;
   let t = inp.length;
@@ -48,7 +47,7 @@ function click() {
   recarg();
 }
 function recarg() {
-  window.onload = document.getElementById("input").value = "";
+  document.getElementById("input").value = "";
 }
 
 function valida() {
@@ -60,7 +59,7 @@ function valida() {
     recarg();
   }
 }
-/////////////////////////////////////////////////////////////
+////////////////////////////////ejercicio13/////////////////////////////
 function click13() {
   let inp13 = document.getElementById("input13").value;
   let cant = inp13.length;
@@ -79,9 +78,9 @@ document.getElementById("input13").addEventListener("input", function () {
   this.style.color = "red";
   this.style.textDecoration = "line-through";
 });
-/////////////////////////////////////////////////////////////
-const t = `<input id="input1" type="text" /><input id="input2" type="text" />
-      <div><input id="input3" type="text" /><input id="input4" type="text" /></div>
+/////////////////////////////////ejercicio14////////////////////////////
+const t = `<input id="input1" type="text" placeholder="Celda1" style="font-style: italic"; /><input id="input2" type="text" placeholder="Celda2" style="font-style:italic"; />
+      <div><input id="input3" type="text" placeholder="Celda3" style="font-style:italic" /><input id="input4" type="text" placeholder="Celda4" style="font-style:italic" /></div>
       <div id="ejerc14"></div>`;
 
 document.getElementById("tab").innerHTML = t;
@@ -91,7 +90,7 @@ function borrar() {
   window.onload = document.getElementById("input3").value = "";
   document.getElementById("input4").value = "";
 }
-/////////////////////////////////////////////////////////////
+////////////////////////////////////ejercicio15/////////////////////////
 let base = Number(document.getElementById("base")).value;
 let altura = Number(document.getElementById("altura")).value;
 
@@ -110,19 +109,24 @@ function volver() {
   document.getElementById("base").value = "";
   document.getElementById("altura").value = "";
 }
-/////////////////////////////////////////////////////////////
+///////////////////////////////////ejercicio16//////////////////////////
 let en = document.getElementById("encendida");
 let apg = document.getElementById("apagada");
 
 function encender() {
+  document.body.style.backgroundColor = "rgb(182, 182, 235)";
+  document.body.style.transition = "background-color 2s ease";
   en.style.display = "block";
   apg.style.display = "none";
 }
 function apagar() {
+  document.body.style.backgroundColor = "rgb(128, 128, 155)";
+  document.body.style.transition = "background-color 0.1s ease";
+
   en.style.display = "none";
   apg.style.display = "block";
 }
-/////////////////////////////////////////////////////////////
+///////////////////////////////////ejercicio17//////////////////////////
 let d = new Date();
 
 const dhias = [
@@ -182,10 +186,15 @@ let horamaya = maya.getHours();
 
 document.getElementById(
   "maya"
-).innerHTML = `Según el calendariomaya el mundo acabaría el ${diasemmaya} ${dimaya} de ${mesmaya}, del año ${añomaya}, a los ${
+).innerHTML = `Según el calendario maya el mundo acabaría el ${diasemmaya} ${dimaya} de ${mesmaya}, del año ${añomaya}, a los ${
   horamaya * 60 * 60 * 1000
 } milisegundos</p>`;
-//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////ejercicio18////////////////////////////////////////
 const ant = new Date(1970);
 const today = new Date();
 // un año = 31,536,000,000 milisegundos///
+let res = (año - 1970) * 31536000000 + di * 60 * 60 * 60 * 1000;
+console.log(res);
+document.getElementById(
+  "segur"
+).innerHTML = `La cantidad de milisegundos que han paasado desde 1970 hasta la fecha es de ${res}`;
