8b6e02c878e08e7a8a1a5e15b4649d219f61c786
Carlo Fidel Taboada Petersson <carloftaboada@gmail.com>
2025-01-30 15:05:10 +0100

    cambios en T3

--- 03-javascript/ejercicios/T3/parimpar.js
+++ 03-javascript/ejercicios/T3/parimpar.js
@@ -1,11 +1,11 @@
-/* let result, resultado;
-const num = document.querySelector("#numero");
+let result, resultado;
+const num1 = document.querySelector("#numero");
 const mostra = document.querySelector("#mostra1");
 const btn1 = document.querySelector("#btn_ejer1");
 const borrar = document.querySelector("#btn_limpiar");
 
 const parimpar = () => {
-  let n = num.value;
+  let n = num1.value;
   console.log(typeof n);
   n = Number(n.replace(","), ".");
 
@@ -15,34 +15,30 @@ const parimpar = () => {
     result = n % 2;
     //Operador ternario
     //Condición a evaluar? acción a ejecutar: (sino) acción descarte
-    result !== 0;
-    resultado = `<div class="alert alert-success"> El vlor introducido es un número impar, concretamente el ${n}<br/>`;
-    resultado = `<div class="alert alert-success"> El vlor introducido es un número par, concretamente el ${n}<br/>`;
+    if (result !== 0) {
+      resultado = `<div class="alert alert-success"> El valor introducido es un número impar, concretamente el ${n}<br/>`;
+    } else {
+      resultado = `<div class="alert alert-success"> El valor introducido es un número par, concretamente el ${n}<br/>`;
+    }
   }
   mostra.innerHTML = resultado;
 };
 
 const borrando = () => {
-  num.value = "";
+  num1.value = "";
   mostra.innerHTML = "";
 };
 
 btn1.onclick = parimpar;
-borrar.onclick = borrando; */
+borrar.onclick = borrando;
 ///////////////////////////////////ejercicio02//////////////////////////////////////////
-let num = document.getElementById("in").value;
+const m = document.querySelector("#mos");
+let num = document.querySelector("#ent");
+num = num.replace(",", ".");
 console.log(num);
 
-function valid() {
-  if (num !== res) {
-    alert("No es el resultado correcto");
-  } else {
-    div();
-    console.log(res);
-  }
-  function div() {
-    let res = 144 / 12;
-  }
+if (isNaN(num) || num == "") {
+  document.querySelector("#inp").innerHTML(`hjkdshfkjsdhfjkhsd`);
 }
 
 ///////////////////////////////////ejercicio03//////////////////////////////////////////
